42fbfa001a052b00b97567bf5235951087a8b129
Anthony RUbio <rubionn27@gmail.com>
2025-08-26 09:27:34 -0500

    Fix: structure

---
+++ module_4/climaTemixco/notebooks/004_ReporteDatos.py
@@ -0,0 +1,16 @@
+
+
+import pandas as pd
+
+from paquete import transforma, concatena, analiza #nombres claros de las funciones
+
+tmx = pd.read
+
+tmx_mensual = 
+tmx_diario =
+f = "/Users/anthonyrubio/Downloads/data/temixco_mensual.csv"; # aclarar narrativamente el archivo
+tmx_mensual.to_csv(f)
+
+
+
+
---
+++ module_4/espacio_trabajo.py
@@ -0,0 +1,25 @@
+
+# El espacio de trabajo es la base fisica del trabajo (Estructura de archivos)
+# El espacio de trabajo se divide en carpetas y subcarpetas 
+
+
+#casaToluca|
+    # ------epw
+    # ------idf
+    # ------notebooks
+    # ------osm
+    # ------resultados
+
+#Analisis de clima
+#climaTemixco
+    # ------data
+        # ------001_raw
+        # ------002_intermedios
+        # ------003_finales
+    # ------notebooks
+        #------paquetes
+    # ------reportes
+
+    
+
+
